7ee6ffea5b9b430f0770726a091f0d97a590fe97
rohit khatana <rohitkhatana.khatana@gmail.com>
2023-05-18 15:31:53 +0530

    working example of embedding with custom context

--- embeddings.py
+++ embeddings.py
@@ -30,3 +30,24 @@ print('reading text')
 embeddings = OpenAIEmbeddings()
 vectordb = Chroma.from_texts(tdd_doc, embeddings, persist_directory=persist_directory)
 vectordb.persist()
+
+tdd_qa = ChatVectorDBChain.from_llm(OpenAI(temperature=0, model_name='gpt-3.5-turbo'), vectordb, return_source_documents=True)
+
+
+query = "what is average SLA"
+chat_history = []
+
+
+def interact_with_tdd():
+    while(True):
+        question = input("ask quesiton(Type no for exit): ")
+        if (question == 'no'):
+            break
+        result = tdd_qa({"question": question, "chat_history": chat_history})
+        print(result["source_documents"])
+        print("-------answer-------")
+        print(result["answer"])
+
+if __name__ == '__main__':
+    interact_with_tdd()
+
